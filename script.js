bce81a7d83c73579241165cfc820611cb82fe5cd
NutyadaS <nutyada.s7810@gmail.com>
2023-03-13 16:31:49 +0700

    add 4 functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// Create function
+function fnAdd(a, b) {
+  let result = a + b;
+  return result;
+}
+function fnSubtract(a, b) {
+  let result = a - b;
+  return result;
+}
+function fnMultiply(a, b) {
+  let result = a * b;
+  return result;
+}
+function fnDivide(a, b) {
+  let result = a / b;
+  return result;
+}
+
+// Create Function Inform
