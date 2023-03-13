124066f46d871a5efc700b0cdc4a8397d319aca6
NutyadaS <nutyada.s7810@gmail.com>
2023-03-13 17:01:32 +0700

    add numbers to function

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // Create function
 function fnAdd(a, b) {
-  let result = a + b;
-  return result;
+  return a + b;
 }
+result = fnAdd(1, 2);
+console.log(result);
+
 function fnSubtract(a, b) {
-  let result = a - b;
-  return result;
+  return a - b;
 }
+result = fnSubtract(1, 2);
+console.log(result);
+
 function fnMultiply(a, b) {
-  let result = a * b;
-  return result;
+  return a * b;
 }
+result = fnMultiply(1, 2);
+console.log(result);
+
 function fnDivide(a, b) {
-  let result = a / b;
-  return result;
+  return a / b;
 }
+result = fnDivide(1, 2);
+console.log(result);
 
 // Create Function Inform
 const myInfo = (firstName, location, hobby) => {
